f072bd34af6b52dc89ba5e946fd786d30d68eadc
Kimber Ogden <mx.kimber@gmail.com>
2023-04-22 16:12:02 -0500

    Add http gem, add to index action to hide api-key

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
 
   def index
-    render json: {message: "working"}
+
+    web_response = HTTP.get("https://newsapi.org/v2/everything?q=starship&apiKey=#{Rails.application.credentials.news_api.api_key}").parse(:json)
+
+    render json: web_response
   end
-
-
 end
+
+
