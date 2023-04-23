775581786a535e4b6399c389b3a27b47a4b2f387
Kimber Ogden <mx.kimber@gmail.com>
2023-04-23 12:09:29 -0500

    Filter API to pull Title starting with 'E'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,14 @@ class ArticlesController < ApplicationController
 
     web_response = HTTP.get("https://newsapi.org/v2/everything?q=starship&apiKey=#{Rails.application.credentials.news_api.api_key}").parse(:json)
 
-    render json: web_response
+    articles = web_response["articles"]
+    filtered_articles = [ ]
+    articles.each do |article|
+      if article["title"][0] == "E"
+        filtered_articles << article
+      end
+    end
+    render json: filtered_articles
   end
 end
 
