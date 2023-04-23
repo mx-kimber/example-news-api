2fa7c7db9f5d80a5ee9750affba1458ea752bb26
Kimber Ogden <mx.kimber@gmail.com>
2023-04-23 13:03:35 -0500

    create user params for a query

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,16 +2,16 @@ class ArticlesController < ApplicationController
 
   def index
 
-    web_response = HTTP.get("https://newsapi.org/v2/everything?q=starship&apiKey=#{Rails.application.credentials.news_api.api_key}").parse(:json)
+    web_response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=#{Rails.application.credentials.news_api.api_key}").parse(:json)
 
     articles = web_response["articles"]
-    filtered_articles = [ ]
-    articles.each do |article|
-      if article["title"][0] == "E"
-        filtered_articles << article
-      end
-    end
-    render json: filtered_articles
+    # filtered_articles = [ ]
+    # articles.each do |article|
+    #   if article["title"][0] == "E"
+    #     filtered_articles << article
+    #   end
+    # end
+    render json: articles
   end
 end
 
